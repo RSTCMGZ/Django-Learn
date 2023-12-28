d06af31dae2056a1df8958d2e2b8c9f014ab650a
Reşat Çamgöz <114862309+RSTCMGZ@users.noreply.github.com>
2023-12-29 00:41:31 +0300

    add-links

--- blog/views.py
+++ blog/views.py
@@ -2,14 +2,50 @@ from django.http.response import HttpResponse
 from django.shortcuts import render
 
 
+data = {
+    'blogs': [
+        {
+            'id': 1,
+            'title':'Web geliştirme',
+            'image': '1.jpg',
+            'is_active': True,
+            'is_home': False,
+            'description': 'harika bir kurs',
+        },
+        {
+            'id': 2,
+            'title':'Python Kursu',
+            'image': '2.jpg',
+            'is_active': True,
+            'is_home': True,
+            'description': 'harika bir kurs',
+        },
+        {
+            'id': 3,
+            'title':'Django geliştirme',
+            'image': '3.jpg',
+            'is_active': False,
+            'is_home': True,
+            'description': 'harika bir kurs',
+        },
+    ]
+}
+
+
 # Create your views here.
 
 def index(request):
-    return render(request,"blog/index.html")
+    context={
+        'blogs':data['blogs']
+    }
+    return render(request,"blog/index.html",context)
 
 
 def blogs(request):
-    return render(request,"blog/blogs.html")
+    context={
+        'blogs':data['blogs']
+    }
+    return render(request,"blog/blogs.html", context)
 
 
 def blog_details(request,id):
