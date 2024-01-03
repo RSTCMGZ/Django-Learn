046a88bd61bc833d6fbf5107bdbf9ec8d07d5c1a
Reşat Çamgöz <114862309+RSTCMGZ@users.noreply.github.com>
2024-01-03 22:54:48 +0300

    many-to-one-learning

--- blog/models.py
+++ blog/models.py
@@ -4,20 +4,6 @@ from ckeditor.fields import RichTextField
 
 
 
-class Blog(models.Model):
-    title = models.CharField(max_length = 200)
-    image = models.ImageField(upload_to="blogs")
-    description = RichTextField()
-    is_active = models.BooleanField(default = False)
-    is_home = models.BooleanField(default = False)
-    slug = models.SlugField(null=False, blank=True,  unique= True, db_index= True, editable=False)
-
-    def __str__(self):
-        return f"{self.title}"
-
-    def save(self, *args, **kwargs):
-        self.slug = slugify(self.title)
-        super().save(*args, **kwargs)
 
 class Category(models.Model):
     name = models.CharField(max_length = 150 )
@@ -30,3 +16,21 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length = 200)
+    image = models.ImageField(upload_to="blogs")
+    description = RichTextField()
+    is_active = models.BooleanField(default = False)
+    is_home = models.BooleanField(default = False)
+    slug = models.SlugField(null=False, blank=True,  unique= True, db_index= True, editable=False)
+    category = models.ForeignKey(Category, default=1, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.title}"
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
--- blog/views.py
+++ blog/views.py
@@ -1,49 +1,22 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
-from blog.models import Blog
-
-data = {
-    'blogs': [
-        {
-            'id': 1,
-            'title':'Web geliştirme',
-            'image': '1.jpg',
-            'is_active': True,
-            'is_home': False,
-            'description': 'harika bir kurs',
-        },
-        {
-            'id': 2,
-            'title':'Node Kursu',
-            'image': '2.jpg',
-            'is_active': True,
-            'is_home': True,
-            'description': 'Dünyada en çok kullanılan bir dil.',
-        },
-        {
-            'id': 3,
-            'title':'Python Kursu',
-            'image': '3.jpg',
-            'is_active': False,
-            'is_home': True,
-            'description': 'Kolay öğrenme imkanı sunar.',
-        },
-    ]
-}
+from blog.models import Blog, Category
 
 
-# Create your views here.
+
 
 def index(request):
     context={
-        'blogs': Blog.objects.filter(is_active = True, is_home = True)
+        "blogs": Blog.objects.filter(is_active = True, is_home = True),
+        "categories": Category.objects.all()
     }
     return render(request,"blog/index.html",context)
 
 
 def blogs(request):
     context={
-        'blogs': Blog.objects.filter(is_active = True)
+        'blogs': Blog.objects.filter(is_active = True),
+        "categories": Category.objects.all()
     }
     return render(request,"blog/blogs.html", context)
 
@@ -52,3 +25,13 @@ def blog_details(request,slug):
 
     blog = Blog.objects.get(slug=slug)
     return render(request,"blog/blog-details.html", {'blog': blog})
+
+
+def blogs_by_category(request,slug):
+    context={
+        'blogs': Blog.objects.filter(is_active = True, category__slug=slug),
+        "categories": Category.objects.all(),
+        "selected_category":slug,
+    }
+    return render(request,"blog/blogs.html", context)
+    
